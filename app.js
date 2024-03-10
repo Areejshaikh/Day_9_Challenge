2b9177640bae49307b66ecfbce7b73b344312d4e
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-03-10 23:27:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 25:
+let alien = "green";
+if (alien === "red") { //condition is false
+    console.log("Game Over!");
+}
+if (alien === "green") {
+    console.log("You are Win!  ");
+}
+// Question 26:
+let alien_color = "red";
+if (alien_color === "red") {
+    console.log("Game Over!");
+}
+if (alien_color === "yellow") {
+    console.log("YOur Score!");
+}
+if (alien_color === "green") {
+    console.log("You are Win!  ");
+}
+else {
+    console.log("Drop");
+}
+// Question 27:
+let aliencolor0 = "red";
+//RED
+if (aliencolor0 === "red") {
+    console.log("YOur score 34");
+}
+if (aliencolor0 === "yellow") {
+    console.log("your score 60");
+}
+if (aliencolor0 === "green") {
+    console.log("Your score 90");
+}
+else {
+    console.log("your score is 0");
+}
+// GREEN
+let aliencolor1 = "green";
+if (aliencolor1 === "red") {
+    console.log("YOur score 34");
+}
+if (aliencolor1 === "yellow") {
+    console.log("your score 60");
+}
+if (aliencolor1 === "green") {
+    console.log("Your score 90");
+}
+else {
+    console.log("your score is 0");
+}
+// YELLOW
+let aliencolor2 = "yellow";
+if (aliencolor2 === "red") {
+    console.log("YOur score 34");
+}
+if (aliencolor2 === "yellow") {
+    console.log("your score 60");
+}
+if (aliencolor2 === "green") {
+    console.log("Your score 90");
+}
+else {
+    console.log("your score is 0");
+}
---
+++ app.ts
@@ -0,0 +1,76 @@
+// Day 9 Challenge: Start Coding!
+import { log } from "console"
+
+// Question 25:
+
+let alien :string = "green";
+if (alien === "red" ) { //condition is false
+    console.log("Game Over!");  
+    
+} if (alien === "green") {
+    console.log("You are Win!  "); 
+} 
+
+// Question 26:
+
+
+let alien_color : string = "red"
+ if (alien_color === "red") {
+    console.log("Game Over!");  
+ }if (alien_color === "yellow") {
+    console.log("YOur Score!");  
+ }if (alien_color === "green") {
+    console.log("You are Win!  "); 
+ } 
+ else{
+    console.log("Drop");
+ }
+
+ // Question 27:
+let aliencolor0: string  = "red";
+
+//RED
+ if (aliencolor0 === "red") {
+    console.log("YOur score 34");
+ }
+ if (aliencolor0 === "yellow") {
+    console.log("your score 60");
+    
+ } if (aliencolor0 === "green") {
+    console.log("Your score 90");
+ } 
+ else {
+    console.log("your score is 0");
+ }
+
+ // GREEN
+
+ let aliencolor1 : string =  "green";
+ if (aliencolor1 === "red") {
+    console.log("YOur score 34");
+ }
+ if (aliencolor1 === "yellow") {
+    console.log("your score 60");
+    
+ } if (aliencolor1 === "green") {
+    console.log("Your score 90");
+ } 
+ else {
+    console.log("your score is 0");
+ }
+
+// YELLOW
+
+ let aliencolor2 : string =  "yellow";
+ if (aliencolor2 === "red") {
+    console.log("YOur score 34");
+ }
+ if (aliencolor2 === "yellow") {
+    console.log("your score 60");
+    
+ } if (aliencolor2 === "green") {
+    console.log("Your score 90");
+ } 
+ else {
+    console.log("your score is 0");
+ }
